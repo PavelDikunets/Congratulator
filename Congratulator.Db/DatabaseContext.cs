f06d0c9218ad3f97dea0a8d345e488f9f56a7166
PavelDikunets <dikunets.pavel@gmail.com>
2023-02-16 16:04:55 +0300

    Settings DatabaseContext Dbset for Person

---
+++ Congratulator.Db/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Congratulator.Db.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Congratulator.Db
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
