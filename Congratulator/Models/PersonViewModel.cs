e07c0d12c12bf2d3b6e9d5df4f05e4659a077bad
PavelDikunets <dikunets.pavel@gmail.com>
2023-02-16 16:02:43 +0300

    Add view models

---
+++ Congratulator/Models/PersonViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Congratulator.Models
+{
+    public partial class PersonViewModel
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public SocialStatusViewModel SocialStatus { get; set; }
+    }
+}
---
+++ Congratulator/Models/SocialStatusViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Congratulator.Models
+{
+    public enum SocialStatusViewModel
+    {
+        [Display(Name = "Знакомые")]
+        Familiar,
+
+        [Display(Name = "Сотрудники")]
+        Employee,
+
+        [Display(Name = "Друзья")]
+        Friend,
+    }
+}
