a28fe40415ba11509b10eb6115558cb1df4edcf1
PavelDikunets <dikunets.pavel@gmail.com>
2023-02-16 16:05:14 +0300

    IPersonsDbRepository

---
+++ Congratulator.Db/IPersonsDbRepository.cs
@@ -0,0 +1,16 @@
+﻿using Congratulator.Db.Models;
+
+namespace Congratulator.Db
+{
+    public interface IPersonsDbRepository
+    {
+        Task<List<Person>> GetAllAsync();
+
+        Task<Person?> TryGetByIdAsync(int id);
+
+        Task AddAsync(Person person);
+
+        Task UpdateAsync(Person editedPerson);
+        Task RemoveAsync(int id);
+    }
+}
