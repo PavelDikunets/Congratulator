716d8fbd06c3b389e8f9935c91bddc0a7c7286eb
PavelDikunets <dikunets.pavel@gmail.com>
2023-02-18 17:25:29 +0300

    Change type string to DateTime for BirthDate Del ctor

--- Congratulator.Db/Models/Person.cs
+++ Congratulator.Db/Models/Person.cs
@@ -1,16 +1,21 @@
-﻿namespace Congratulator.Db.Models
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Congratulator.Db.Models
 {
     public class Person
     {
         public int Id { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? BirthDate { get; set; }
-        public SocialStatus SocialStatus { get; set; }
 
-        public Person()
-        {
-            SocialStatus = SocialStatus.Familiar;
-        }
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        [Column(TypeName = "date")]
+        [DataType(DataType.Date)]
+        public DateTime? BirthDate { get; set; }
+
+        public SocialStatus Status { get; set; }
     }
 }
