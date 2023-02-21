75d5b727a68f3d95a18e4f30694840cad4ed535b
PavelDikunets <dikunets.pavel@gmail.com>
2023-02-21 17:34:49 +0300

    Impl PersonViewModel

--- Congratulator/Models/PersonViewModel.cs
+++ Congratulator/Models/PersonViewModel.cs
@@ -1,11 +1,28 @@
-﻿namespace Congratulator.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Congratulator.WebApp.Models
 {
     public partial class PersonViewModel
     {
         public int Id { get; set; }
+
+        [Required]
         public string? FirstName { get; set; }
+
+        [Required]
         public string? LastName { get; set; }
-        public DateOnly BirthDate { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string? ImagePath { get; set; }
+
+        public IFormFile? UploadedFIle { get; set; }
+
         public SocialStatusViewModel SocialStatus { get; set; }
+
+        public PersonViewModel()
+        {
+            SocialStatus = SocialStatusViewModel.Familiar;
+        }
     }
 }
