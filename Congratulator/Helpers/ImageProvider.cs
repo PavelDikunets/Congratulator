391920d6e18ad5b31dca45d634cac09b1dd7275a
PavelDikunets <dikunets.pavel@gmail.com>
2023-02-24 21:26:26 +0300

    Add an ImageProvider to upload images to the server

---
+++ Congratulator/Helpers/ImageProvider.cs
@@ -0,0 +1,33 @@
+﻿namespace Congratulator.WebApp.Helpers
+{
+    public class ImageProvider
+    {
+        private readonly IWebHostEnvironment _appEnvironment;
+
+        public ImageProvider(IWebHostEnvironment appEnvironment)
+        {
+            _appEnvironment = appEnvironment;
+        }
+
+        public string? SaveFile(IFormFile file)
+        {
+            if (file != null)
+            {
+                var folderPath = Path.Combine(_appEnvironment.WebRootPath + "/images/avatars/");
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+
+                var fileName = Guid.NewGuid() + "." + file.FileName.Split('.').Last();
+                var path = Path.Combine(folderPath, fileName);
+                using (var fileStream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(fileStream);
+                }
+                return "/images/avatars/" + fileName;
+            }
+            return null;
+        }
+    }
+}
