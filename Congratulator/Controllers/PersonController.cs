7d84c22e89994dca0066fc0adc332abda28982d5
PavelDikunets <dikunets.pavel@gmail.com>
2023-02-24 21:29:03 +0300

    Add avatar uploading implementation

--- Congratulator/Controllers/PersonController.cs
+++ Congratulator/Controllers/PersonController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Congratulator.Db;
 using Congratulator.Db.Models;
+using Congratulator.WebApp.Helpers;
 using Congratulator.WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,13 +10,14 @@ namespace Congratulator.WebApp.Controllers
     public class PersonController : Controller
     {
         private readonly IPersonsDbRepository _personsDbRepository;
+        private readonly ImageProvider _imageProvider;
         private readonly IMapper _mapper;
 
-
-        public PersonController(IPersonsDbRepository personsDbRepository, IMapper mapper)
+        public PersonController(IPersonsDbRepository personsDbRepository, IMapper mapper, ImageProvider imageProvider)
         {
             _personsDbRepository = personsDbRepository;
             _mapper = mapper;
+            _imageProvider = imageProvider;
         }
 
         public async Task<IActionResult> IndexAsync()
@@ -38,9 +40,15 @@ namespace Congratulator.WebApp.Controllers
                 ModelState.AddModelError("", "Пожалуйста, заполните все поля!");
                 return View(model);
             }
+
+            var imagePath = _imageProvider.SaveFile(model.UploadedFile);
+            if (imagePath != null)
+            {
+                model.ImagePath = imagePath;
+            }
             var personDb = _mapper.Map<Person>(model);
             await _personsDbRepository.AddAsync(personDb);
-            return RedirectToAction("Index", "Person");
+            return RedirectToAction("Index", "Home");
         }
 
         public async Task<IActionResult> EditAsync(int Id)
@@ -58,15 +66,22 @@ namespace Congratulator.WebApp.Controllers
                 ModelState.AddModelError("", "Пожалуйста, заполните все поля!");
                 return View(model);
             }
+
+            var imagePath = _imageProvider.SaveFile(model.UploadedFile);
+            if (imagePath != null)
+            {
+                model.ImagePath = imagePath;
+            }
+
             var personDb = _mapper.Map<Person>(model);
             await _personsDbRepository.UpdateAsync(personDb);
-            return RedirectToAction("Index", "Person");
+            return RedirectToAction("Edit", "Person");
         }
 
         public async Task<IActionResult> RemoveAsync(int Id)
         {
             await _personsDbRepository.RemoveAsync(Id);
-            return RedirectToAction("Index", "Person");
+            return RedirectToAction("Index", "Home");
         }
     }
 }
