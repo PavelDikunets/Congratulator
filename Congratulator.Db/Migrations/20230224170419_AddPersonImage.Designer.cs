c5dbc7867f96ea83459966230d8a0a5ca6c4448a
PavelDikunets <dikunets.pavel@gmail.com>
2023-02-24 21:59:52 +0300

    Add Database migration

--- Congratulator.Db/Migrations/20230224170419_AddPersonImage.Designer.cs
+++ Congratulator.Db/Migrations/20230224170419_AddPersonImage.Designer.cs
@@ -4,6 +4,7 @@ using Congratulator.Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Congratulator.Db.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230224170419_AddPersonImage")]
+    partial class AddPersonImage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Congratulator.Db/Migrations/20230224170419_AddPersonImage.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Congratulator.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPersonImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImagePath = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageOfPersonId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Images_Persons_ImageOfPersonId",
+                        column: x => x.ImageOfPersonId,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Images",
+                columns: new[] { "Id", "ImageOfPersonId", "ImagePath" },
+                values: new object[,]
+                {
+                    { 1, 1, "/images/avatars/default.jpg" },
+                    { 2, 2, "/images/avatars/default.jpg" },
+                    { 3, 3, "/images/avatars/default.jpg" },
+                    { 4, 4, "/images/avatars/default.jpg" },
+                    { 5, 5, "/images/avatars/default.jpg" },
+                    { 6, 6, "/images/avatars/default.jpg" },
+                    { 7, 7, "/images/avatars/default.jpg" },
+                    { 8, 8, "/images/avatars/default.jpg" },
+                    { 9, 9, "/images/avatars/default.jpg" },
+                    { 10, 10, "/images/avatars/default.jpg" },
+                    { 11, 11, "/images/avatars/default.jpg" },
+                    { 12, 12, "/images/avatars/default.jpg" },
+                    { 13, 13, "/images/avatars/default.jpg" },
+                    { 14, 14, "/images/avatars/default.jpg" },
+                    { 15, 15, "/images/avatars/default.jpg" },
+                    { 16, 16, "/images/avatars/default.jpg" },
+                    { 17, 17, "/images/avatars/default.jpg" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_ImageOfPersonId",
+                table: "Images",
+                column: "ImageOfPersonId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Images");
+        }
+    }
+}
