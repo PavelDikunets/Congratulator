4899c63942732f81aba6b5f93a8bace9ffed2085
PavelDikunets <dikunets.pavel@gmail.com>
2023-02-24 22:00:16 +0300

    Add PersonImage

---
+++ Congratulator.Db/Models/PersonImage.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Congratulator.Db.Models
+{
+    public class PersonImage
+    {
+
+        public int Id { get; set; }
+        public string ImagePath { get; set; }
+
+        public int ImageOfPersonId { get; set; }
+        public Person Person { get; set; }
+    }
+}
