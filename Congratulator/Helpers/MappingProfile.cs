65d4af84e757e19b5c9aa43118b548c8c85f4a00
PavelDikunets <dikunets.pavel@gmail.com>
2023-02-24 21:26:48 +0300

    Add mapping Avatar

--- Congratulator/Helpers/MappingProfile.cs
+++ Congratulator/Helpers/MappingProfile.cs
@@ -6,9 +6,10 @@ namespace Congratulator.WebApp.Helpers
 {
     public class MappingProfile : Profile
     {
-        public MappingProfile() 
+        public MappingProfile()
         {
-            CreateMap<Person, PersonViewModel>().ReverseMap();
-        } 
+            CreateMap<Person, PersonViewModel>().IncludeMembers(u => u.Avatar).ReverseMap();
+            CreateMap<PersonImage, PersonViewModel>(MemberList.None).ReverseMap();
+        }
     }
 }
